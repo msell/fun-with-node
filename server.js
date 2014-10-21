f5dac84d48543b28a15d47f98cdbd03132b5bc2f
Matt Sell <mwsell@gmail.com>
2014-10-20 21:21:26 -0500

    add middleware that logs time of request using moment

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
+var moment = require('moment');
 var app = express();
 
-app.get('/', function(req,res,next){
+
+// a middleware with no mount path; gets executed for every request to app
+app.use(function(req, res, next){
+	console.log('Time:', moment().format('MMMM Do YYYY, h:mm:ss a'));
+	next();
+});
+
+app.get('/', function(req,res){
 	res.send('Hello World');
 });
 
